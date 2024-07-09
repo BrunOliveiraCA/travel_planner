287238e0883b0e6ff29ae37eea88c85b811e9875
Bruno Luis de Oliveira <brunoliveira.ca@gmail.com>
2024-07-09 09:58:35 -0300

    configs: Adding framework to project and config .vscode

---
+++ run.py
@@ -0,0 +1,7 @@
+'''
+    Este módulo inicializa o servidor Flask.
+'''
+from src.main.server.server import app
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=3000, debug=True)
---
+++ src/main/server/server.py
@@ -0,0 +1,7 @@
+'''
+    Este módulo cria um servidor flask.
+'''
+
+from flask import Flask
+
+app = Flask(__name__)

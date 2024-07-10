c44a1f850e850d14302c798d6ba0d4388e3133bc
Bruno Luis de Oliveira <brunoliveira.ca@gmail.com>
2024-07-10 14:35:33 -0300

    feat: Implementing links_repository with tests

--- src/models/repositories/emails_to_invite_repository.py
+++ src/models/repositories/emails_to_invite_repository.py
@@ -28,6 +28,6 @@ class EmailsToInviteRepository:
                 SELECT * FROM emails_to_invite WHERE trip_id = ?
             ''', (trip_id,)
         )
-        trip = cursor.fetchall()
-        return trip
+        emails = cursor.fetchall()
+        return emails
     
